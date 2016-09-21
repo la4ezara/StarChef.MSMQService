4e2c70ad243edfb4c6d0e35527f6d130bb23450b
Tihomir Nikolov <Tihomir.Nikolov@fourth.com>
2016-09-21 10:48:27 +0300

    Include new install files

---
+++ Install/install.bat
@@ -0,0 +1,20 @@
+@echo off
+echo.
+echo Be sure to run this command as Administrator
+echo First argument should be service name.
+echo Second argument should be service location.
+echo.
+
+set serviceName="StarChef.MSMQService"
+set servicePath=%1
+set installUtilPath="C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\installutil"
+
+echo Try to install service "%serviceName%"
+%installUtilPath% /serviceName=%serviceName% %servicePath%
+
+for /F "tokens=3 delims=: " %%H in ('sc query %serviceName%^| findstr "STATE"') do (
+  if /I "%%H" NEQ "RUNNING" (
+	echo Service "%serviceName%" found.
+	net start %serviceName%
+  )
+)
