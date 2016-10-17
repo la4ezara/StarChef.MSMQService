9623f07093df2136a5285b3e1b4fd264f241733a
saumil-d-shah <saumil.shah@fourth.com>
2016-10-17 13:26:53 +0100

    US21864 - MealPeriodSender

---
+++ StarChef.Common/Models/Customer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StarChef.Common.Models
+{
+    public class Customer
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string ExternalId { get; set; }
+
+        public Customer(int CustomerId)
+        {
+            var dbManager = new DatabaseManager();
+
+            Id = CustomerId;
+            string connectionString = ConfigurationManager.AppSettings["DSN"];
+
+            var reader = dbManager.ExecuteReader(connectionString,
+                                    "sc_get_database_details",
+                                    new SqlParameter("@db_database_id", CustomerId));
+            if(reader.Read())
+            {
+                ExternalId = reader[1].ToString();
+                Name = reader[2].ToString();
+            }
+        }
+
+    }
+}
