4a699ec9523d00b018cb176197d85c20c4eea43a
Tihomir Nikolov <Tihomir.Nikolov@fourth.com>
2016-10-20 17:54:24 +0300

    update install files

--- StarChef.Listener/Install/uninstall.bat
+++ StarChef.Listener/Install/uninstall.bat
@@ -9,12 +9,13 @@ set servicePath=%1
 set installUtilPath="C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\installutil"
 
 if EXIST %servicePath% ( 
+	echo "Path found %servicePath%."
 	for /F "tokens=3 delims=: " %%H in ('sc query %serviceName%^| findstr "STATE"') do (
 	  if /I "%%H" EQU "RUNNING" (
 		echo Service "%serviceName%" found - try to stop service.
 		net stop %serviceName%
 	  )
-  
+	echo "Uninstall service %serviceName%"
 	  %installUtilPath% /serviceName=%serviceName% /u %servicePath%
 	)
 ) ELSE (
