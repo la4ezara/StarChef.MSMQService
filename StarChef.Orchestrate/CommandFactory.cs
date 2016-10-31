499f481f1f1952b0dfa6d2eabf84ed4d731415a1
LKostadinova <LKostadinova@SOF-7040-008>
2016-10-31 10:56:52 +0200

    US22318 Create a Publisher For New Modified SC User

---
+++ StarChef.Orchestrate/CommandFactory.cs
@@ -0,0 +1,62 @@
+﻿using StarChef.Orchestrate.Models;
+using System;
+using Fourth.Orchestration.Model.Examples;
+using Commands = Fourth.Orchestration.Model.People.Commands;
+
+namespace StarChef.Orchestrate
+{
+    public class CommandFactory
+    {
+        public static Commands.ActivateAccount ActivateAccountCommand(string dbConnectionString, int entityId, int databaseId)
+        {
+            Customer cust = new Customer(databaseId);
+            User u = new User(entityId);
+
+            var builder = u.BuildActivateAccount(cust, dbConnectionString);
+
+            // Build the immutable data object
+            var eventObj = builder.Build();
+
+            return eventObj;
+        }
+
+        public static Commands.DeactivateAccount DeactivateAccountCommand(string dbConnectionString, int entityId, int databaseId)
+        {
+            Customer cust = new Customer(databaseId);
+            User u = new User(entityId);
+
+            var builder = u.BuildDeactivateAccount(cust, dbConnectionString);
+
+            // Build the immutable data object
+            var eventObj = builder.Build();
+
+            return eventObj;
+        }
+
+        public static Commands.CreateAccount CreateAccountCommand(string dbConnectionString, int entityId, int databaseId)
+        {
+            Customer cust = new Customer(databaseId);
+            User u = new User(entityId);
+
+            var builder = u.BuildCreateAccount(cust, dbConnectionString);
+
+            // Build the immutable data object
+            var eventObj = builder.Build();
+
+            return eventObj;
+        }
+
+        public static Commands.UpdateAccount UpdateAccountCommand(string dbConnectionString, int entityId, int databaseId)
+        {
+            Customer cust = new Customer(databaseId);
+            User u = new User(entityId);
+
+            var builder = u.BuildUpdateAccount(cust, dbConnectionString);
+
+            // Build the immutable data object
+            var eventObj = builder.Build();
+
+            return eventObj;
+        }
+    }
+}
---
+++ StarChef.Orchestrate/Models/User.cs
@@ -0,0 +1,121 @@
+﻿using StarChef.Common;
+using System.Data.SqlClient;
+using System;
+using Fourth.Orchestration.Model.Examples;
+using Events = Fourth.Orchestration.Model.Menus.Events;
+using Commands = Fourth.Orchestration.Model.People.Commands;
+
+namespace StarChef.Orchestrate.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+
+
+        public User(int userId)
+        {
+            Id = userId;
+        }
+
+        public Events.UserUpdated.Builder Build(Customer cust, string connectionString)
+        {
+            var rand = new Random();
+            var builder = Events.UserUpdated.CreateBuilder();
+            var dbManager = new DatabaseManager();
+
+            var reader = dbManager.ExecuteReader(connectionString,
+                                    "sc_event_user",
+                                    new SqlParameter("@entity_id", Id));
+            if (reader.Read())
+            {
+                builder.SetCustomerId(cust.ExternalId)
+                .SetCustomerName(cust.Name)
+                .SetExternalId(reader[1].ToString())
+                .SetFirstName(reader[4].ToString())
+                .SetLastName(reader[5].ToString())
+                .SetLanguage(reader[10].ToString())
+                .SetSource(Events.SourceSystem.STARCHEF)
+                .SetSequenceNumber(rand.Next(1, int.MaxValue));
+            }
+            return builder;
+        }
+
+        public Commands.ActivateAccount.Builder BuildActivateAccount(Customer cust, string connectionString)
+        {
+            var rand = new Random();
+            var builder = Commands.ActivateAccount.CreateBuilder();
+            var dbManager = new DatabaseManager();
+
+            var reader = dbManager.ExecuteReader(connectionString,
+                                    "sc_event_user",
+                                    new SqlParameter("@entity_id", Id));
+            if (reader.Read())
+            {
+                builder.SetExternalId(cust.ExternalId)
+                        .SetCommandId(rand.Next(1, int.MaxValue).ToString())
+                        .SetSource(Commands.SourceSystem.STARCHEF);
+            }
+            return builder;
+        }
+
+        public Commands.DeactivateAccount.Builder BuildDeactivateAccount(Customer cust, string connectionString)
+        {
+            var rand = new Random();
+            var builder = Commands.DeactivateAccount.CreateBuilder();
+            var dbManager = new DatabaseManager();
+
+            var reader = dbManager.ExecuteReader(connectionString,
+                                    "sc_event_user",
+                                    new SqlParameter("@entity_id", Id));
+            if (reader.Read())
+            {
+                builder.SetExternalId(cust.ExternalId)
+                        .SetCommandId(rand.Next(1, int.MaxValue).ToString())
+                        .SetSource(Commands.SourceSystem.STARCHEF);
+            }
+            return builder;
+        }
+
+        public Commands.CreateAccount.Builder BuildCreateAccount(Customer cust, string connectionString)
+        {
+            var rand = new Random();
+            var builder = Commands.CreateAccount.CreateBuilder();
+            var dbManager = new DatabaseManager();
+
+            var reader = dbManager.ExecuteReader(connectionString,
+                                    "sc_event_user",
+                                    new SqlParameter("@entity_id", Id));
+            if (reader.Read())
+            {
+                builder.SetCustomerId(cust.ExternalId)
+                        .SetEmailAddress(reader[2].ToString())
+                        .SetFirstName(reader[4].ToString())
+                        .SetLastName(reader[5].ToString())
+                        .SetSource(Commands.SourceSystem.STARCHEF);
+            }
+            return builder;
+        }
+
+        public Commands.UpdateAccount.Builder BuildUpdateAccount(Customer cust, string connectionString)
+        {
+            var rand = new Random();
+            var builder = Commands.UpdateAccount.CreateBuilder();
+            var dbManager = new DatabaseManager();
+
+            var reader = dbManager.ExecuteReader(connectionString,
+                                    "sc_event_user",
+                                    new SqlParameter("@entity_id", Id));
+            if (reader.Read())
+            {
+                builder.SetCustomerId(cust.ExternalId)
+                        .SetExternalId(reader[1].ToString())
+                        .SetEmailAddress(reader[2].ToString())
+                        .SetFirstName(reader[4].ToString())
+                        .SetLastName(reader[5].ToString())
+                        .SetCommandId(rand.Next(1, int.MaxValue).ToString())
+                        .SetSource(Commands.SourceSystem.STARCHEF);
+            }
+            return builder;
+        }
+    }
+}
