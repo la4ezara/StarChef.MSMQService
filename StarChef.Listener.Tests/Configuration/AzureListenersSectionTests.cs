55c73d38b4d25d9d4dc3fef776c064f6e78b817b
Alexander Goida <alexander.goida@gmail.com>
2016-11-01 22:46:12 +0200

    Added tests for the configuration section

---
+++ StarChef.Listener.Tests/Configuration/AzureListenersSectionTests.cs
@@ -0,0 +1,35 @@
+﻿using StarChef.Listener.Configuration;
+using StarChef.Listener.Tests.Configuration.Handlers;
+using Xunit;
+
+namespace StarChef.Listener.Tests.Configuration
+{
+    public class AzureListenersSectionTests
+    {
+        [Fact(DisplayName = "Should load configuration with single listener and handler")]
+        public void Should_read_configuration_section_with_one_item()
+        {
+            var config = AzureListenersSection.GetConfiguration("simple");
+            Assert.Equal(1, config.Count);
+            Assert.Equal(typeof(FirstHandlerType), config["subscription1"][0]);
+        }
+
+        [Fact(DisplayName = "Should load configuration with several handlers for the same listener")]
+        public void Should_read_configuration_section_with_several_handlers()
+        {
+            var config = AzureListenersSection.GetConfiguration("twoHandlers");
+            Assert.Equal(1, config.Count);
+            Assert.Equal(typeof(FirstHandlerType), config["subscription1"][0]);
+            Assert.Equal(typeof(SecondHandlerType), config["subscription1"][1]);
+        }
+
+        [Fact(DisplayName = "Should load configuration with several listener/handlers")]
+        public void Should_read_configuration_section_with_several_subscriptions()
+        {
+            var config = AzureListenersSection.GetConfiguration("twoSubscriptions");
+            Assert.Equal(2, config.Count);
+            Assert.Equal(typeof(FirstHandlerType), config["subscription1"][0]);
+            Assert.Equal(typeof(SecondHandlerType), config["subscription2"][0]);
+        }
+    }
+}
---
+++ StarChef.Listener.Tests/Configuration/Handlers/FirstHandlerType.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Threading.Tasks;
+using Fourth.Orchestration.Messaging;
+using Fourth.Orchestration.Model.Recipes;
+
+namespace StarChef.Listener.Tests.Configuration.Handlers
+{
+    public class FirstHandlerType : IMessageHandler<Events.PriceBandUpdated>
+    {
+        public Task<MessageHandlerResult> HandleAsync(Events.PriceBandUpdated payload, string trackingId)
+        {
+            return null;
+        }
+    }
+}
