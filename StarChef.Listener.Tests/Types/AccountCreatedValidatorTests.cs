826884cf37ed67921f5e6a5703ac68d8466c4205
Alexander Goida <alexander.goida@gmail.com>
2016-11-07 00:15:36 +0200

    Added tests for validator

---
+++ StarChef.Listener.Tests/Types/AccountCreatedValidatorTests.cs
@@ -0,0 +1,95 @@
+﻿using AccountCreated = Fourth.Orchestration.Model.People.Events.AccountCreated;
+using StarChef.Listener.Types;
+using Xunit;
+
+namespace StarChef.Listener.Tests.Types
+{
+    public class AccountCreatedValidatorTests
+    {
+        [Fact]
+        public void Should_return_true_for_valid_model()
+        {
+            var builder = AccountCreated.CreateBuilder();
+            builder
+                .SetInternalId("1")
+                .SetFirstName("1")
+                .SetLastName("1")
+                .SetEmailAddress("1")
+                .SetExternalId("1");
+            var accountCreated = builder.Build();
+
+            var validator = new AccountCreatedValidator();
+            var actual = validator.IsValid(accountCreated);
+
+            Assert.True(actual);
+        }
+
+        [Fact]
+        public void Should_return_false_when_InternalId_missing()
+        {
+            var builder = AccountCreated.CreateBuilder();
+            builder
+                .SetFirstName("1")
+                .SetLastName("1")
+                .SetEmailAddress("1")
+                .SetExternalId("1");
+            var accountCreated = builder.Build();
+
+            var validator = new AccountCreatedValidator();
+            var actual = validator.IsValid(accountCreated);
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void Should_return_false_when_FirstName_missing()
+        {
+            var builder = AccountCreated.CreateBuilder();
+            builder
+                .SetInternalId("1")
+                .SetLastName("1")
+                .SetEmailAddress("1")
+                .SetExternalId("1");
+            var accountCreated = builder.Build();
+
+            var validator = new AccountCreatedValidator();
+            var actual = validator.IsValid(accountCreated);
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void Should_return_false_when_LastName_missing()
+        {
+            var builder = AccountCreated.CreateBuilder();
+            builder
+                .SetInternalId("1")
+                .SetFirstName("1")
+                .SetEmailAddress("1")
+                .SetExternalId("1");
+            var accountCreated = builder.Build();
+
+            var validator = new AccountCreatedValidator();
+            var actual = validator.IsValid(accountCreated);
+
+            Assert.False(actual);
+        }
+
+        [Fact]
+        public void Should_return_false_when_EmailAddress_missing()
+        {
+            var builder = AccountCreated.CreateBuilder();
+            builder
+                .SetInternalId("1")
+                .SetFirstName("1")
+                .SetLastName("1")
+                .SetExternalId("1");
+            var accountCreated = builder.Build();
+
+            var validator = new AccountCreatedValidator();
+            var actual = validator.IsValid(accountCreated);
+
+            Assert.False(actual);
+        }
+    }
+}
