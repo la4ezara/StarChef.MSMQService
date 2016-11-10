42435402f3831d6aec73155f116433d36fea5ea2
Amit Rai Sharma <amit.sharma@fourth.com>
2016-11-10 14:16:02 +0000

    initial commit

---
+++ DataExport/DataExported/Form1.Designer.cs
@@ -0,0 +1,137 @@
+﻿namespace DataExported
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.btnRetrieve = new System.Windows.Forms.Button();
+            this.txtConnectionString = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dgEntityInfo = new System.Windows.Forms.DataGridView();
+            this.button1 = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dgEntityInfo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.btnRetrieve);
+            this.panel1.Controls.Add(this.txtConnectionString);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(733, 64);
+            this.panel1.TabIndex = 0;
+            // 
+            // btnRetrieve
+            // 
+            this.btnRetrieve.Location = new System.Drawing.Point(630, 27);
+            this.btnRetrieve.Name = "btnRetrieve";
+            this.btnRetrieve.Size = new System.Drawing.Size(94, 31);
+            this.btnRetrieve.TabIndex = 6;
+            this.btnRetrieve.Text = "Retrieve Data";
+            this.btnRetrieve.UseVisualStyleBackColor = true;
+            // 
+            // txtConnectionString
+            // 
+            this.txtConnectionString.Location = new System.Drawing.Point(75, 1);
+            this.txtConnectionString.Name = "txtConnectionString";
+            this.txtConnectionString.Size = new System.Drawing.Size(649, 20);
+            this.txtConnectionString.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Database:";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.button1);
+            this.panel2.Controls.Add(this.dgEntityInfo);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.panel2.Location = new System.Drawing.Point(0, 64);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(733, 515);
+            this.panel2.TabIndex = 7;
+            // 
+            // dgEntityInfo
+            // 
+            this.dgEntityInfo.AllowUserToAddRows = false;
+            this.dgEntityInfo.AllowUserToDeleteRows = false;
+            this.dgEntityInfo.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgEntityInfo.Location = new System.Drawing.Point(12, 16);
+            this.dgEntityInfo.Name = "dgEntityInfo";
+            this.dgEntityInfo.Size = new System.Drawing.Size(688, 150);
+            this.dgEntityInfo.TabIndex = 0;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(30, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(733, 579);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dgEntityInfo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.TextBox txtConnectionString;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnRetrieve;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dgEntityInfo;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ DataExport/DataExported/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace DataExported
+{
+    public enum EntityEnum
+    {
+        Menu,
+        Group,
+        Recipe,
+        User,
+        MealPeriod
+    };
+
+    public partial class Form1 : Form
+    {
+        private SortedDictionary<EntityEnum, QueryDetails> Entities { get; }
+
+        private ConcurrentDictionary<EntityEnum, DataTable> QueryData { get; set; }
+
+        public Form1()
+        {
+
+            this.Entities = this.MapEntities();
+            this.QueryData = new ConcurrentDictionary<EntityEnum, DataTable>();
+
+            InitializeComponent();
+            var ds = this.MapEntities();
+            txtConnectionString.Text =
+                @"Initial Catalog=SCNET_abokado;Data Source=10.10.10.109\DEVTEST;User ID=sl_web_user; Password=reddevil;";
+
+            this.btnRetrieve.Click += btnRetrieve_Click;
+        }
+
+        
+        private async void btnRetrieve_Click(object sender, EventArgs e)
+        {
+            await Task.WhenAll(this.GetQueries());
+            var l = new List<EntityInfo>();
+            foreach (var q in this.QueryData)
+            {
+                l.Add(new EntityInfo
+                {
+                    Name = q.Key.ToString(),
+                    RowCount = q.Value.Rows.Count,
+                    Selected = false
+                });
+            }
+            dgEntityInfo.DataSource = new BindingSource(new BindingList<EntityInfo>(l), null);
+        }
+
+        private List<Task> GetQueries()
+        {
+            var tasks = new List<Task>();
+
+            foreach (var e in this.Entities)
+            {
+                tasks.Add(this.ExecuteQuery(txtConnectionString.Text, e.Value));
+            }
+
+            return tasks;
+        }
+
+        private SortedDictionary<EntityEnum, QueryDetails> MapEntities()
+        {
+            var entities = new SortedDictionary<EntityEnum, QueryDetails>
+            {
+                {
+                    EntityEnum.Group, new QueryDetails
+                    {
+                        Name = EntityEnum.Group,
+                        Query = "select * from [Group]"
+                    }
+                },
+                {
+                    EntityEnum.Menu, new QueryDetails
+                    {
+                        Name = EntityEnum.Menu,
+                        Query = "select * from [Menu]"
+                    }
+                },
+                {
+                    EntityEnum.Recipe, new QueryDetails
+                    {
+                        Name = EntityEnum.Recipe,
+                        Query = "select * from [User]"
+                    }
+                },
+                {
+                    EntityEnum.User, new QueryDetails
+                    {
+                        Name = EntityEnum.User,
+                        Query = "select * from [User]"
+                    }
+                },
+                {
+                    EntityEnum.MealPeriod, new QueryDetails
+                    {
+                        Name = EntityEnum.MealPeriod,
+                        Query = "select * from [meal_period]"
+                    }
+                }
+            };
+
+            return entities;
+        }
+
+
+        private async Task ExecuteQuery(string connectionString, QueryDetails query)
+        {
+            using (var cn = new SqlConnection(connectionString))
+            {
+                cn.Open();
+
+                var cmd = new SqlCommand(query.Query, cn);
+
+                var reader = await cmd.ExecuteReaderAsync();
+                var dt = new DataTable();
+                dt.Load(reader);
+                this.QueryData[query.Name] = dt;
+            }
+        }
+
+        private void AddMessage()
+        {
+            
+        }
+
+        class EntityInfo
+        {
+            public string Name { get; set; }
+            public long RowCount { get; set; }
+            public bool Selected { get; set; }
+        }
+
+        class QueryDetails
+        {
+            public EntityEnum Name { get; set; }
+            public string Query { get; set; }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+
+}
