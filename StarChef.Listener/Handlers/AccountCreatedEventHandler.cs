6a6f485e5c8b163cbf2a0afbb4b7e54d4036a2ae
LKostadinova <LKostadinova@SOF-7040-008>
2017-03-27 11:56:47 +0300

    update

--- StarChef.Listener/Handlers/AccountCreatedEventHandler.cs
+++ StarChef.Listener/Handlers/AccountCreatedEventHandler.cs
@@ -42,7 +42,6 @@ namespace StarChef.Listener.Handlers
                     var user = Mapper.Map<AccountCreatedTransferObject>(payload);
                     try
                     {
-                        await DbCommands.AddUser(user);//!!!!!!!!! to be removed
                         await DbCommands.UpdateExternalId(user);
                         await MessagingLogger.MessageProcessedSuccessfully(payload, trackingId);
                         _logger.Processed(trackingId, payload);
