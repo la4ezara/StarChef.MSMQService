e795a4e657ccd93ba088c48ad2ec987abe80ee6a
Alexander Goida <alexander.goida@gmail.com>
2017-04-04 16:42:14 +0300

    Made Jenkins happier

--- StarChef.Listener/Install/install.bat
+++ StarChef.Listener/Install/install.bat
@@ -1,6 +1,19 @@
 @echo off
 echo.
 echo Be sure to run this command as Administrator
+echo First argument should be service location.
 echo.
-C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\installutil "..\StarChef.Listener.exe"
-net start StarchefMessageListner
+
+set serviceName="StarchefMessageListner"
+set servicePath=%1
+set installUtilPath="C:\WINDOWS\Microsoft.NET\Framework\v4.0.30319\installutil"
+
+echo Try to install service %serviceName%
+%installUtilPath% /serviceName=%serviceName% %servicePath%
+
+for /F "tokens=3 delims=: " %%H in ('sc query %serviceName%^| findstr "STATE"') do (
+  if /I "%%H" NEQ "RUNNING" (
+	echo Service "%serviceName%" found.
+	net start %serviceName%
+  )
+)
