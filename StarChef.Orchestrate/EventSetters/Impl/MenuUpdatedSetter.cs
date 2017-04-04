2207e53a145a5a029425240e15e844482d6246b3
Alexander Goida <alexander.goida@gmail.com>
2017-04-04 14:57:28 +0300

    Revert "Merge pull request #69 from fourth/US29108"
    This reverts commit 5cef7e2df38e9092be7109f114b4d7190ae545d4, reversing
    changes made to 1faff3d54d402ec62eeb3e7f378a371c0c3e921c.

--- StarChef.Orchestrate/EventSetters/Impl/MenuUpdatedSetter.cs
+++ StarChef.Orchestrate/EventSetters/Impl/MenuUpdatedSetter.cs
@@ -37,13 +37,6 @@ namespace StarChef.Orchestrate
                     .SetExternalId(reader[1].ToString())
                     .SetMenuName(reader[2].ToString())
                     .SetMenuType((byte)reader[3] == 1 ? Events.MenuType.ALACARTE : Events.MenuType.BUFFET);
-
-                //This is for Menu Buffet Sales 
-                var menuCountName = "menu_count";
-                if (!reader.IsDBNull(menuCountName))
-                {
-                    builder.SetBuffetMenuSales(reader.GetValue<double>(menuCountName));
-                }
             }
 
             if (reader.NextResult())
@@ -66,14 +59,6 @@ namespace StarChef.Orchestrate
                         .SetRecipeName(reader[1].ToString())
                         .SetCourseName(reader[2].ToString())
                         .SetDisplayOrder((int)reader[3]);
-
-                    //This is for menu buffet Sales Mix
-                    var sellQuantityName = "sell_quantity";
-                    if (!reader.IsDBNull(sellQuantityName))
-                    {
-                        recipeBuilder.SetSellQuantity(reader.GetValue<double>(sellQuantityName));
-                    }
-
                     builder.AddRecipes(recipeBuilder);
                 }
             }
