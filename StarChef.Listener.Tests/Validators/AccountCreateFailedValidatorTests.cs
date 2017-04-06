021184be4a8bdf1aeecc282b46a06b858cbebad7
Alexander Goida <alexander.goida@gmail.com>
2017-04-06 17:39:28 +0300

    Empty code parameter is initialized now

--- StarChef.Listener.Tests/Validators/AccountCreateFailedValidatorTests.cs
+++ StarChef.Listener.Tests/Validators/AccountCreateFailedValidatorTests.cs
@@ -30,5 +30,20 @@ namespace StarChef.Listener.Tests.Validators
             Assert.False(result);
             Assert.Equal("InternalId is not Int32: " + MORE_THAN_INT, validator.GetErrors());
         }
+
+        [Fact]
+        public void Should_return_true_for_valid_model()
+        {
+            var builder = AccountCreateFailed.CreateBuilder();
+            builder
+                .SetInternalId("1")
+                .SetReason(AccountCreateFailedReason.INVALID_CREATE_DATA);
+            var accountCreated = builder.Build();
+
+            var validator = new AccountCreateFailedValidator(Mock.Of<IDatabaseCommands>());
+            var actual = validator.IsValidPayload(accountCreated);
+
+            Assert.True(actual);
+        }
     }
 }
