69f56972c1700fe0d009b7a15f1001cfe028fd38
Krasimir Georgiev <Krasimir.Georgiev@fourth.com>
2019-06-06 10:20:20 +0300

    Fix

--- StarChef.Orchestrate/EventSetters/Impl/RecipeUpdatedSetter.cs
+++ StarChef.Orchestrate/EventSetters/Impl/RecipeUpdatedSetter.cs
@@ -318,7 +318,7 @@ namespace StarChef.Orchestrate
         {
             public int Id { get; set; }
             public string Name { get; set; }
-            public long SetTypeId { get; set; }
+            public byte SetTypeId { get; set; }
         }
     }
 }
