ad0dad1d18e073f4a49553e352a53f0ddb9b5a90
Tihomir Nikolov <Tihomir.Nikolov@fourth.com>
2019-11-14 17:41:42 +0200

    Update packages and extension

--- StarChef.Common/Extensions/TypeExtensions.cs
+++ StarChef.Common/Extensions/TypeExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Fourth.StarChef.Invariables;
 using Newtonsoft.Json;
 
 namespace StarChef.Common.Extensions
@@ -12,6 +13,14 @@ namespace StarChef.Common.Extensions
         public static string ToJson(this object obj)
         {
             return obj == null ? string.Empty : JsonConvert.SerializeObject(obj);
+        }
+
+        public static string ToJson(this UpdateMessage obj)
+        {
+            UpdateMessage cloneMsg = new UpdateMessage(obj);
+            cloneMsg.DSN = string.Empty;
+
+            return obj == null ? string.Empty : JsonConvert.SerializeObject(cloneMsg);
         }
     }
 }
