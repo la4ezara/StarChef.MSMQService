283bbe16ded5d0abba126143d377d97e734e8ad8
Tihomir Nikolov <tihomirpn@gmail.com>
2020-11-04 17:25:21 +0200

    Increase timeout

--- StarChef.BackgroundServices.Common/Jobs/ICreatePackageJob.cs
+++ StarChef.BackgroundServices.Common/Jobs/ICreatePackageJob.cs
@@ -1,10 +1,12 @@
-﻿using StarChef.BackgroundServices.Common.Attributes;
+﻿using Hangfire;
+using StarChef.BackgroundServices.Common.Attributes;
 using System;
 
 namespace StarChef.BackgroundServices.Common.Jobs
 {
     [DefaultJobLogging]
     [CustomDisableConcurrentExecution(timeoutInSeconds: 300)]
+    [Queue("packages")]
     public interface ICreatePackageJob
     {
         void Process(int databaseId, int packageId, int userId, Guid uniqueIdent);
--- StarChef.BackgroundServices.Common/Jobs/IFileImportJob.cs
+++ StarChef.BackgroundServices.Common/Jobs/IFileImportJob.cs
@@ -1,9 +1,11 @@
-﻿using StarChef.BackgroundServices.Common.Attributes;
+﻿using Hangfire;
+using StarChef.BackgroundServices.Common.Attributes;
 
 namespace StarChef.BackgroundServices.Common.Jobs
 {
     [DefaultJobLogging]
     [CustomDisableConcurrentExecution(timeoutInSeconds: 1800)]
+    [Queue("critical")]
     public interface IFileImportJob
     {
         void Process(int databaseId, string externalId, string filePath, int importId);
