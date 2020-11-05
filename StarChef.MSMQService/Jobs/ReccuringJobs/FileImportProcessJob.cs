3f3c62871ed81688249334ca39162243f820f5d0
Tihomir Nikolov <tihomirpn@gmail.com>
2020-11-05 11:07:08 +0200

    Update app config connections

--- StarChef.MSMQService/Jobs/ReccuringJobs/FileImportProcessJob.cs
+++ StarChef.MSMQService/Jobs/ReccuringJobs/FileImportProcessJob.cs
@@ -17,7 +17,7 @@ namespace StarChef.MSMQService.Jobs.ReccuringJobs
             try
             {
                 Logger.Info($"Processing FileImportProcessJob {msg}");
-                var result = _importService.ImportFileUploaded(filePath, "SL_login");
+                var result = _importService.ImportFileUploaded(filePath, "StarChefLogin");
                 Logger.Info($"Processed FileImportProcessJob {msg}");
             }
             catch (Exception e) {
