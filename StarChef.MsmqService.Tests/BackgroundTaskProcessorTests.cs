206e746e4de4ef028c53e77bdf73422a55b2b9f3
Tihomir Nikolov <tihomirpn@gmail.com>
2020-11-06 17:45:23 +0200

    Update tests and logic

---
+++ StarChef.MsmqService.Tests/BackgroundTaskProcessorTests.cs
@@ -0,0 +1,44 @@
+﻿using Fourth.StarChef.Invariables;
+using log4net;
+using Moq;
+using StarChef.Common;
+using StarChef.Common.Types;
+using StarChef.MSMQService;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace StarChef.MsmqService.Tests
+{
+    public class BackgroundTaskProcessorTests
+    {
+        [Fact]
+        [Trait("Background Tasks", "AAA")]
+        public void AAA() {
+
+            var settings = new Dictionary<string, ImportTypeSettings>();
+            settings.Add("IngredientPriceBand", new ImportTypeSettings() { AutoCalculateCost = true, AutoCalculateIntolerance = true, Id = 1, Name = "IngredientPriceBand" });
+
+            var dbManagerMock = new Mock<IDatabaseManager>();
+            var logMock = new Mock<ILog>();
+            dbManagerMock.Setup(x=> x.GetImportSettings(It.IsAny<string>(), It.IsAny<int>())).Returns(() => { return settings; });
+
+            BackgroundTaskProcessor processor = new BackgroundTaskProcessor(1, string.Empty, dbManagerMock.Object, logMock.Object);
+            BackgroundTask task = new BackgroundTask()
+            {
+                TaskType = Constants.MessageActionType.EntityImported,
+                SubTaskType = Constants.MessageSubActionType.ImportedIngredientPriceBand,
+                ExtendedProperties = @"{'PRICE_BANDS':'55'}"
+            };
+
+            task.ExtendedProperties = string.Empty;
+            task.ExtendedProperties = @"{'PRICE_BANDS':''}";
+
+            processor.ProcessMessage(task);
+            Assert.True(true);
+        }
+    }
+}
